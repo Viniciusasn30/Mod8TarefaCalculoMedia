49cf16c9c734e4f9305b4aab27b03ad371ef76ec
Viniciusasn30 <145036925+Viniciusasn30@users.noreply.github.com>
2023-11-07 15:21:12 -0300

    Update ExercicioModOito.java

--- eclipse-workspace/Exerciciomod8/src/exercicio/mod8/ExercicioModOito.java
+++ eclipse-workspace/Exerciciomod8/src/exercicio/mod8/ExercicioModOito.java
@@ -9,11 +9,12 @@ public class ExercicioModOito {
 	}
 	
 	public static void operacoesNotas(){
-		int nota1 = 10 / 4;
-		double nota2 = 9.5 / 4 ;
-		double nota3 = 9.5 / 4;
-		int nota4 = 9 / 4;
-		double media1 = nota1 + nota2 + nota3 + nota4 ;
+		int nota1 = 10;
+		double nota2 = 9.5;
+		double nota3 = 9.5;
+		int nota4 = 9;
+		double nota5 = nota1 + nota2 + nota3 + nota4 ;
+		double media1 = nota5 / 4;
 
 		System.out.println("****Media**** ");
 
